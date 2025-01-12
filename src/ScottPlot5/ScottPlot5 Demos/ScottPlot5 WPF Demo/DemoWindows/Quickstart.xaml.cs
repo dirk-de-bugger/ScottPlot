d9438495abe8052aed6296fc966dbac2d8c68c50
Dirk "DeBugger <floesselaal@gmail.com>
2025-01-12 18:49:06 +0100

    Update Quickstart.xaml.cs
    Add sample QuickstartViewModel

--- src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WPF Demo/DemoWindows/Quickstart.xaml.cs
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 WPF Demo/DemoWindows/Quickstart.xaml.cs
@@ -16,3 +16,26 @@ public partial class Quickstart : Window, IDemoWindow
         WpfPlot1.Plot.Add.Signal(ScottPlot.Generate.Cos());
     }
 }
+
+public class QuickstartViewModel
+{
+    WpfPlot? _wpfPlot1 = null;
+    public WpfPlot? WpfPlot1Property
+    {
+        get => _wpfPlot1;
+
+        set
+        {
+            // Note: set will be called twice.
+            // First with value==null on load / creation (at InitializeComponent)
+            // Second by WpfPlot with the proper instance value (at DispatcherPriority.Loaded,
+            // if Application.Current?.Dispatcher exists and runs).
+
+            _wpfPlot1 = value;
+
+            // Thus null-checks are necessary (also in any other places that read _wpfPlot1 or WpfPlot1Property):
+            _wpfPlot1?.Plot.Add.Signal(ScottPlot.Generate.SquareWave(cycles:5, pointsPerCycle:10));
+            _wpfPlot1?.Refresh();
+        }
+    }
+}
